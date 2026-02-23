d052ee1bf62360973825736dcddfc0aac72b5753
Miko975 <mikaillaa431@gmail.com>
2026-02-23 23:51:23 +0100

    presentation.tsx

--- components/presentation.tsx
+++ components/presentation.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Presentation() {
+export const Presentation = () => {
   return (
     <div>
       <h1>Test de secours</h1>
       <p>Si ce message s'affiche, c'est qu'on a gagné une bataille contre Vercel !</p>
     </div>
   );
-}
+};
