7e305ff6f7705216333f432f1bea3723be6a8419
Miko975 <mikaillaa431@gmail.com>
2026-02-23 23:20:32 +0100

    presentation.tsx

--- components/presentation.tsx
+++ components/presentation.tsx
@@ -1,1 +1,10 @@
-
+export function Presentation() {
+return (
+<section id="presentation" className="py-20">
+<div className="container mx-auto">
+<h2 className="text-3xl font-bold">Mon Parcours</h2>
+<p className="mt-4">Etudiant en BTS SIO option SISR.</p>
+</div>
+</section>
+)
+}
