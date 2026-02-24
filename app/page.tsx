363976e88bcae61134259e5e3a2ca8e62dbc1499
Miko975 <mikaillaa431@gmail.com>
2026-02-24 01:09:13 +0100

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Hero />
       
       {/* On crée un conteneur pour tout le reste avec un espacement (gap) important */}
-      <div className="flex flex-col gap-32 md:gap-48 pb-20">
+      <div className="flex flex-col gap-32 md:gap-42 pb-20">
         <Presentation />
         <BentoGrid />
         <About />
