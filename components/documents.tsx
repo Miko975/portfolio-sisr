517c66587671a0fc588f8dde21b19931db1d814a
Miko975 <mikaillaa431@gmail.com>
2026-03-09 13:40:01 +0100

    documents.tsx

--- components/documents.tsx
+++ components/documents.tsx
@@ -37,7 +37,7 @@ export function Documents() {
         <p className="mb-4 text-sm font-black uppercase tracking-[0.3em] text-cyan-400">
           Ressources
         </p>
-        <h2 className="text-3xl font-bold text-white tracking-tight italic">Documents Officiels</h2>
+        <h2 className="text-3xl font-bold text-white tracking-tight italic">Documents</h2>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
